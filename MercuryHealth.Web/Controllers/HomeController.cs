7356621f2dafc353ab24cd23cb76147d6f9435a4
Randy Pagels <rpagels@microsoft.com>
2022-01-31 11:48:01 -0500

    Updates

--- MercuryHealth.Web/Controllers/HomeController.cs
+++ MercuryHealth.Web/Controllers/HomeController.cs
@@ -1,21 +1,62 @@
-﻿using MercuryHealth.Web.Models;
+﻿using MercuryHealth.Web.Data;
+using MercuryHealth.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+//using Microsoft.FeatureManagement;
 using System.Diagnostics;
 
 namespace MercuryHealth.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
+        private readonly MercuryHealthWebContext _context;
+        // readonly IFeatureManager _featureManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public IConfiguration Configuration { get; set; }
+
+        //        public HomeController(ILogger<HomeController> logger)
+        //public HomeController(MercuryHealthWebContext context, IConfiguration config, IFeatureManagerSnapshot featureManager)
+        public HomeController(MercuryHealthWebContext context, IConfiguration config)
+
         {
-            _logger = logger;
+            //_featureManager = featureManager;
+            _context = context;
+            Configuration = config;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Todo: Chris & Randy Stuff to do next!
+            //
+            // Mock setup for Config
+            // 
+            ViewData["myEnvironment"] = Configuration["Environment"];
+
+            List<AccessLogs> ObjAccessLogs = new List<AccessLogs>();
+
+            // Insert new record for each page visit
+            var query = new AccessLogs
+            {
+                PageName = "Home",
+                AccessDate = DateTime.UtcNow
+            };
+
+            _context.AccessLogs.Add(query);
+            _context.SaveChanges();
+
+            int pagecount = (_context.AccessLogs.Where(x => x.PageName.Equals("Home")).Count());
+            query = _context.AccessLogs.OrderByDescending(x => x.AccessDate).FirstOrDefault();
+            DateTime pagevisit = query.AccessDate;
+
+            AccessLogs Obj = new AccessLogs();
+            Obj.PageName = "Home";
+            Obj.AccessDate = pagevisit;
+            Obj.Visits = pagecount;
+            ObjAccessLogs.Add(Obj);
+
+            return View(ObjAccessLogs.ToList());
+
+            //return View();
         }
 
         public IActionResult Privacy()
