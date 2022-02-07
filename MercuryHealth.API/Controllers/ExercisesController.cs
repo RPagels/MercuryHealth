f9f90b62756ac948ec8886f88f8e9e9db76d4fe1
Randy Pagels <rpagels@microsoft.com>
2022-02-07 17:09:17 -0600

    API Updates

---
+++ MercuryHealth.API/Controllers/ExercisesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MercuryHealth.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExercisesController : ControllerBase
+    {
+        // GET: api/<ExercisesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ExercisesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ExercisesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ExercisesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ExercisesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MercuryHealth.FunctionApp/InsertExerciseRecord.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace MercuryHealth.FunctionApp;
+
+public class InsertExerciseRecord
+{
+    private static string ApimSubscriptionKey = System.Environment.GetEnvironmentVariable("ApimSubscriptionKey");
+
+    [FunctionName("InsertExerciseRecord")]
+    public async Task RunAsync([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+    {
+        // Time Trigger Cheat Sheet: https://codehollow.com/2017/02/azure-functions-time-trigger-cron-cheat-sheet/
+        // 0 * * * * *	    every minute
+        // 0 */5 * * * *	every 5 minutes
+        // 0 0 */6 * * *	every 6 hours
+
+        //log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+        var client = new HttpClient();
+        var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+        // Request headers with APIM Key retrieved from Azure KeyVault
+        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", ApimSubscriptionKey);
+
+        var uri = "https://rpagels-apim.azure-api.net/api/Exercises?" + queryString;
+
+        HttpResponseMessage response;
+
+        string serializedObject = Newtonsoft.Json.JsonConvert.SerializeObject(new
+        {
+            id = "0",
+            name = "Running/Jogging",
+            description = "Build strong bones and improves cardiovascular.",
+            ExerciseTime = DateTime.Now,
+            musclesInvolved = "Legs",
+            equipment = "None"
+        });
+
+        // Request body
+        byte[] byteData = Encoding.UTF8.GetBytes(serializedObject);
+
+        using (var content = new ByteArrayContent(byteData))
+        {
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            response = await client.PostAsync(uri, content);
+
+            // Get the JSON response.
+            string contentString = await response.Content.ReadAsStringAsync();
+
+            // Display the JSON response.
+            Console.WriteLine("\nResponse:\n");
+            Console.WriteLine(contentString);
+
+        }
+    }
+}
