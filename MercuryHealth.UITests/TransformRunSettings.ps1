c99fe59f53df3fbf89bd722421550e30a6440dc0
Randy Pagels <rpagels@microsoft.com>
2022-03-11 15:26:10 -0500

    Update TransformRunSettings.ps1

--- MercuryHealth.UITests/TransformRunSettings.ps1
+++ MercuryHealth.UITests/TransformRunSettings.ps1
@@ -3,16 +3,21 @@ param($website_url)
 
 Write-Host "Start-Transforming Website URL"
 
+Write-Host "Open runsettings file"
 # Get the runsettings file
 $Content = Get-Content .\MercuryHealth.UITests\MercuryHealthTests.runsettings
 
+Write-Host "Replace Text in runsettings file"
 # Replace Text
-$Content.replace(‘WebSiteUrlHere’,’$website_url’) | Set-Content .\MercuryHealth.UITests\MercuryHealthTests.runsettings
+$Content.replace(‘WebSiteUrlHere’,’$website_url’)
 
-# Update File
+Write-Host "Updated the runsettings file"
+# Updated the runsettings file
+Set-Content .\MercuryHealth.UITests\MercuryHealthTests.runsettings
+
+Write-Host "Open runsettings file"
+# Get the runsettings file
 Get-Content .\MercuryHealth.UITests\MercuryHealthTests.runsettings
-
-#Write-Host "Website URL: " $Content
 
 Write-Host "End-Transforming Website URL"
 
