d18792dd19ecf2792e8520670e4773819f64365d
Randy Pagels <rpagels@microsoft.com>
2022-03-11 15:10:54 -0500

    Update TransformRunSettings.ps1

--- MercuryHealth.UITests/TransformRunSettings.ps1
+++ MercuryHealth.UITests/TransformRunSettings.ps1
@@ -1,23 +1,18 @@
-#Get the new Website URL
+# Get the new Website URL
 param($website_url)
 
 Write-Host "Start-Transforming Website URL"
 
-#Get the version from the csproj file
-#$xml = [Xml] (Get-Content .\MercuryHealth.Web\MercuryHealth.Web.csproj)
-#$initialVersion = [Version] $xml.Project.PropertyGroup.Version # Cannot convert the "System.Object[]" value of type "System.Object[]" to type "System.Version".
-
-# Test ONLY
-#$spliteVersionTemp = $initialVersion.split(".") #ERROR: Method invocation failed because [System.Version] does not contain a method named 'split'.
-#Write-Host "Split Version Test Only: " $spliteVersionTemp
-#$initialVersion ="1.3.0.0"
-
-#Get the runsettings file
+# Get the runsettings file
 $Content = Get-Content .\MercuryHealth.UITests\MercuryHealthTests.runsettings
+
+# Replace Text
 $Content.replace(‘WebSiteUrlHere’,’$website_url’) | Set-Content .\MercuryHealth.UITests\MercuryHealthTests.runsettings
+
+# Update File
 Get-Content .\MercuryHealth.UITests\MercuryHealthTests.runsettings
 
-Write-Host "Website URL: " $Content
+#Write-Host "Website URL: " $Content
 
 Write-Host "End-Transforming Website URL"
 
